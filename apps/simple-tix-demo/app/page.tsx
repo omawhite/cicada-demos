6d3ffa86867c1c984a025b97da972efbc0ecf7a1
Omar Louis White <louiswhite3019@icloud.com>
2025-10-26 22:45:05 -0400

    feat: adds banner

--- apps/simple-tix-demo/app/page.tsx
+++ apps/simple-tix-demo/app/page.tsx
@@ -1,11 +1,15 @@
 import { Button } from "@workspace/ui/components/button";
+import Banner from "../components/Banner";
 
 export default function Page() {
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Simple Tix Demo</h1>
-        <Button size="sm">Button</Button>
+    <div className="min-h-svh">
+      <Banner />
+      <div className="flex items-center justify-center py-16">
+        <div className="flex flex-col items-center justify-center gap-4">
+          <h1 className="text-2xl font-bold">Simple Tix Demo</h1>
+          <Button size="sm">Button</Button>
+        </div>
       </div>
     </div>
   );
