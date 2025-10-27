b3b3ea4ed13d140b3c272afbd9934c4fd84131bc
Omar Louis White <louiswhite3019@icloud.com>
2025-10-26 22:09:19 -0400

    chore: adds cicada logos feat: adds headers

--- apps/simple-tix-demo/app/layout.tsx
+++ apps/simple-tix-demo/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
 import "@workspace/ui/globals.css";
+import Header from "@/components/Header";
 import { Providers } from "@/components/providers";
 
 const fontSans = Geist({
@@ -29,7 +30,10 @@ export default function RootLayout({
 			<body
 				className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}
 			>
-				<Providers>{children}</Providers>
+				<Providers>
+					<Header />
+					{children}
+				</Providers>
 			</body>
 		</html>
 	);
